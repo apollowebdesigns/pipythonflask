7bab6541063f7a0aac8b92d4445d702b8c8797d3
Andy Evans <andrewevans0@outlook.com>
2016-09-19 20:38:45 +0100

    built api to python framework

--- flasktest.py
+++ flasktest.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
 import RPi.GPIO as GPIO
 import datetime
 import time
 app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 @app.route("/")
 def red():
