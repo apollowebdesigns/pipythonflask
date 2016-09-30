60b5b2667675fd9001d66810291ab62119be2b83
Andy Evans <andrewevans0@outlook.com>
2016-09-30 17:42:14 +0100

    java added for pi

---
+++ javapi/pijava/src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+/**
+ * Created by andrewevans on 30/09/2016.
+ */
+package hello;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
