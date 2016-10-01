ea8e7ca13525ba8f1fcccb0e89d8941cbb0df667
Andy Evans <andrewevans0@outlook.com>
2016-10-01 11:01:51 +0100

    service made to handle http requests

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -23,3 +23,26 @@ function AppController($http) {
 
     
 }
+
+
+//TODO move into seperate service file, check carefully with style guide
+
+angular
+    .module('app')
+    .service('ledService', ledService);
+
+ledService.$inject = ['$http'];
+
+function ledService ($http) {
+
+    this.getData = _getData;
+
+    function _getData(color) {
+        //TODO check link is correct over router as hits wernt connecting
+        $http.get("http://192.168.1.67:9876/hits/" + color)
+        .then(function(response) {
+            console.log('data received');
+            vm.requestedData = response.data;
+        });
+    }
+}
