d243e1d5347c7f3a0dc45d7969fb38adf9b7709b
Andy Evans <andrewevans0@outlook.com>
2016-11-02 15:47:43 +0000

    server now added for light

--- bluetestled.py
+++ bluetestled.py
@@ -14,13 +14,16 @@ LED = 2
 
 #########################
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(LED,GPIO.OUT)
-print "LED on"
-GPIO.output(LED,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(LED,GPIO.LOW)
+@app.route('/hits/motor', methods=['GET'])
+def motor_move():
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(LED,GPIO.OUT)
+    print "LED on"
+    GPIO.output(LED,GPIO.HIGH)
+    time.sleep(1)
+    print "LED off"
+    GPIO.output(LED,GPIO.LOW)
 
 ###TODO set up server and client to test led, then move service from python to javascript (or java)
