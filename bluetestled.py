515e766d88fbe076145fcd15aa3a91631098308a
Andy Evans <andrewevans0@outlook.com>
2016-11-02 15:47:12 +0000

    modding server

--- bluetestled.py
+++ bluetestled.py
@@ -1,5 +1,10 @@
+from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
+import datetime
 import time
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+app = Flask(__name__)
 
 #setting constants
 
