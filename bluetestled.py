f079bdcf052bfc217e18d5eee03c0d950f424c93
Andy Evans <andrewevans0@outlook.com>
2016-11-03 21:32:35 +0000

    server added

--- bluetestled.py
+++ bluetestled.py
@@ -4,14 +4,17 @@ import datetime
 import time
 from time import sleep
 
+@app.route('/hits/blue', methods=['GET'])
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(17,GPIO.OUT)
+    print "LED on"
+    GPIO.output(17,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(17,GPIO.LOW)
+    print "LED off"
+    GPIO.output(17,GPIO.LOW)
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-print "LED on"
-GPIO.output(17,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(17,GPIO.LOW)
-print "LED off"
-GPIO.output(17,GPIO.LOW)
-GPIO.setmode(GPIO.BCM)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=9877, debug=True)
