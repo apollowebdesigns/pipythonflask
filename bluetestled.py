2b216174e138c98866bc9894f07450ad85f3490c
Andy Evans <andrewevans0@outlook.com>
2016-11-03 21:29:42 +0000

    pins changed

--- bluetestled.py
+++ bluetestled.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(2,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 print "LED on"
-GPIO.output(2,GPIO.HIGH)
+GPIO.output(17,GPIO.HIGH)
 time.sleep(1)
-GPIO.output(2,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
 print "LED off"
-GPIO.output(2,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
 GPIO.setmode(GPIO.BCM)
