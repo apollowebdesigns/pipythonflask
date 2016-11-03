0e1967e495f6f60ba5609679bc147dc13562ad75
Andy Evans <andrewevans0@outlook.com>
2016-11-03 21:25:11 +0000

    changed

--- bluetestled.py
+++ bluetestled.py
@@ -1,31 +1,10 @@
-from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
-import datetime
 import time
-from time import sleep
-GPIO.setmode(GPIO.BOARD)
-app = Flask(__name__)
-
-#setting constants
-
-#########################
-
-LED = 2
-
-#########################
-
-@app.route('/hits/blue', methods=['GET'])
-def light_shine():
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(LED,GPIO.OUT)
-    print "LED on"
-    GPIO.output(LED,GPIO.HIGH)
-    time.sleep(1)
-    print "LED off"
-    GPIO.output(LED,GPIO.LOW)
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=9877, debug=True)
-###TODO set up server and client to test led, then move service from python to javascript (or java)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2,GPIO.OUT)
+print "LED on"
+GPIO.output(2,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(2,GPIO.LOW)
