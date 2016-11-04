91610d5f16058b1dc18d62c2b7ef44c882a372f7
Andy Evans <andrewevans0@outlook.com>
2016-11-04 18:37:58 +0000

    server changed

--- bluetestled.py
+++ bluetestled.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 @app.route('/hits/blue', methods=['GET'])
 def lights():
     GPIO.setwarnings(False)
-    GPIO.setup(17,GPIO.OUT)
+    GPIO.setup(18,GPIO.OUT)
     print "LED on"
-    GPIO.output(17,GPIO.HIGH)
+    GPIO.output(18,GPIO.HIGH)
     time.sleep(1)
-    GPIO.output(17,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
     print "LED off"
-    GPIO.output(17,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
     GPIO.setmode(GPIO.BCM)
     return "hello pi"
 
