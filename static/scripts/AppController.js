76f72ba75e470437f73b20ade73d30274b3a0666
Andy Evans <andrewevans0@outlook.com>
2016-11-11 19:24:22 +0000

    controller linked up

--- static/scripts/AppController.js
+++ static/scripts/AppController.js
@@ -9,6 +9,8 @@ function AppController(ledService, driveService) {
     var vm = this;
     vm.requestedData = 'test data';
     vm.getData = ledService.getData;
+    vm.driveForwards = driveService.driveForwards;
+    vm.driveData = driveService.driveData;
 
     vm.driveData = driveService.driveData;
     
