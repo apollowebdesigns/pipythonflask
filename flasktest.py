be46f247e96f5601e64fe62dba0f449efecaabb1
Andy Evans <andrewevans0@outlook.com>
2016-11-13 13:01:18 +0000

    conaining the functions

--- flasktest.py
+++ flasktest.py
@@ -35,39 +35,41 @@ CORS(app)
 
 @app.route('/hits/forwards', methods=['GET'])
 def move_forwards():
-    Motor1A = 16
-    Motor1B = 18
-    Motor1E = 22
-    
-    Motor2A = 23
-    Motor2B = 21
-    Motor2E = 19
 
-    GPIO.setup(Motor1A,GPIO.OUT)
-    GPIO.setup(Motor1B,GPIO.OUT)
-    GPIO.setup(Motor1E,GPIO.OUT)
-    
-    GPIO.setup(Motor2A,GPIO.OUT)
-    GPIO.setup(Motor2B,GPIO.OUT)
-    GPIO.setup(Motor2E,GPIO.OUT)
-    
-    print "Going forwards"
-    GPIO.output(Motor1A,GPIO.HIGH)
-    GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor1E,GPIO.HIGH)
-    
-    GPIO.output(Motor2A,GPIO.HIGH)
-    GPIO.output(Motor2B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.HIGH)
-    
-    sleep(10)
+    def motor():
+        Motor1A = 16
+        Motor1B = 18
+        Motor1E = 22
+        
+        Motor2A = 23
+        Motor2B = 21
+        Motor2E = 19
 
-    print "Now stop"
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
-    
-    GPIO.cleanup()
-    return "test"
+        GPIO.setup(Motor1A,GPIO.OUT)
+        GPIO.setup(Motor1B,GPIO.OUT)
+        GPIO.setup(Motor1E,GPIO.OUT)
+        
+        GPIO.setup(Motor2A,GPIO.OUT)
+        GPIO.setup(Motor2B,GPIO.OUT)
+        GPIO.setup(Motor2E,GPIO.OUT)
+        
+        print "Going forwards"
+        GPIO.output(Motor1A,GPIO.HIGH)
+        GPIO.output(Motor1B,GPIO.LOW)
+        GPIO.output(Motor1E,GPIO.HIGH)
+        
+        GPIO.output(Motor2A,GPIO.HIGH)
+        GPIO.output(Motor2B,GPIO.LOW)
+        GPIO.output(Motor2E,GPIO.HIGH)
+        
+        sleep(10)
+
+        print "Now stop"
+        GPIO.output(Motor1E,GPIO.LOW)
+        GPIO.output(Motor2E,GPIO.LOW)
+        
+        GPIO.cleanup()
+    return move_forwards()
 
 
 
