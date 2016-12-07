eb7932033e5065138a7fef46f3c49dd92cd1ca46
Andy Evans <andrewevans0@outlook.com>
2016-12-07 18:56:22 +0000

    removing and tidying up files

--- javascriptpintest.js
+++
@@ -1,8 +0,0 @@
-var gpio = require("pi-gpio");
-var pin = 17;
-
-gpio.open(pin, "output", function(err) {		// Open pin 17 for output 
-    gpio.write(pin, 1, function() {			// Set pin 17 high (1) 
-        gpio.close(pin);						// Close pin 17 
-    });
-});
