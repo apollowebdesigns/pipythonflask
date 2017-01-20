eda6fc966c2cf00f225734b170082484dc06df02
Andy Evans <andrewevans0@outlook.com>
2017-01-20 19:24:54 +0000

    moving python routes into separate modules

--- flasktest.py
+++ flasktest.py
@@ -4,6 +4,7 @@ import datetime
 import time
 from time import sleep
 from flask_cors import CORS, cross_origin
+from motormove import motor_move
 GPIO.setmode(GPIO.BOARD)
 app = Flask(__name__)
 CORS(app)
@@ -50,56 +51,7 @@ def move_forwards():
 
 
 @app.route('/hits/motor', methods=['GET'])
-def motor_move():
-
-    def motor_test():
-        
-        Motor1A = 16
-        Motor1B = 18
-        Motor1E = 22
-        
-        Motor2A = 23
-        Motor2B = 21
-        Motor2E = 19
-        
-        GPIO.setup(Motor1A,GPIO.OUT)
-        GPIO.setup(Motor1B,GPIO.OUT)
-        GPIO.setup(Motor1E,GPIO.OUT)
-        
-        GPIO.setup(Motor2A,GPIO.OUT)
-        GPIO.setup(Motor2B,GPIO.OUT)
-        GPIO.setup(Motor2E,GPIO.OUT)
-        
-        print "Going forwards"
-        GPIO.output(Motor1A,GPIO.HIGH)
-        GPIO.output(Motor1B,GPIO.LOW)
-        GPIO.output(Motor1E,GPIO.HIGH)
-        
-        GPIO.output(Motor2A,GPIO.HIGH)
-        GPIO.output(Motor2B,GPIO.LOW)
-        GPIO.output(Motor2E,GPIO.HIGH)
-        
-        sleep(2)
-        
-        print "Going backwards"
-        GPIO.output(Motor1A,GPIO.LOW)
-        GPIO.output(Motor1B,GPIO.HIGH)
-        GPIO.output(Motor1E,GPIO.HIGH)
-        
-        GPIO.output(Motor2A,GPIO.LOW)
-        GPIO.output(Motor2B,GPIO.HIGH)
-        GPIO.output(Motor2E,GPIO.HIGH)
-        
-        sleep(2)
-        
-        print "Now stop"
-        GPIO.output(Motor1E,GPIO.LOW)
-        GPIO.output(Motor2E,GPIO.LOW)
-        
-        GPIO.cleanup()
-        return
-    
-    return motor_test()
+motor_move()
 
 
 ####test
