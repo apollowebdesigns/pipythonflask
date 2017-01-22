ba933e78e71c6f8bff907d61220a080de2bc5c74
Andy Evans <andrewevans0@outlook.com>
2017-01-22 08:03:47 +0000

    class added in python

--- motormove.py
+++ motormove.py
@@ -1,48 +1,53 @@
-def motor_move():
-    def motor_test():
-        Motor1A = 16
-        Motor1B = 18
-        Motor1E = 22
+class motor_move:
 
-        Motor2A = 23
-        Motor2B = 21
-        Motor2E = 19
+    def __init__(self):
+        self.test = "hello I am a test"
 
-        GPIO.setup(Motor1A, GPIO.OUT)
-        GPIO.setup(Motor1B, GPIO.OUT)
-        GPIO.setup(Motor1E, GPIO.OUT)
+    def motor_move():
+        def motor_test():
+            Motor1A = 16
+            Motor1B = 18
+            Motor1E = 22
 
-        GPIO.setup(Motor2A, GPIO.OUT)
-        GPIO.setup(Motor2B, GPIO.OUT)
-        GPIO.setup(Motor2E, GPIO.OUT)
+            Motor2A = 23
+            Motor2B = 21
+            Motor2E = 19
 
-        print "Going forwards"
-        GPIO.output(Motor1A, GPIO.HIGH)
-        GPIO.output(Motor1B, GPIO.LOW)
-        GPIO.output(Motor1E, GPIO.HIGH)
+            GPIO.setup(Motor1A, GPIO.OUT)
+            GPIO.setup(Motor1B, GPIO.OUT)
+            GPIO.setup(Motor1E, GPIO.OUT)
 
-        GPIO.output(Motor2A, GPIO.HIGH)
-        GPIO.output(Motor2B, GPIO.LOW)
-        GPIO.output(Motor2E, GPIO.HIGH)
+            GPIO.setup(Motor2A, GPIO.OUT)
+            GPIO.setup(Motor2B, GPIO.OUT)
+            GPIO.setup(Motor2E, GPIO.OUT)
 
-        sleep(2)
+            print "Going forwards"
+            GPIO.output(Motor1A, GPIO.HIGH)
+            GPIO.output(Motor1B, GPIO.LOW)
+            GPIO.output(Motor1E, GPIO.HIGH)
 
-        print "Going backwards"
-        GPIO.output(Motor1A, GPIO.LOW)
-        GPIO.output(Motor1B, GPIO.HIGH)
-        GPIO.output(Motor1E, GPIO.HIGH)
+            GPIO.output(Motor2A, GPIO.HIGH)
+            GPIO.output(Motor2B, GPIO.LOW)
+            GPIO.output(Motor2E, GPIO.HIGH)
 
-        GPIO.output(Motor2A, GPIO.LOW)
-        GPIO.output(Motor2B, GPIO.HIGH)
-        GPIO.output(Motor2E, GPIO.HIGH)
+            sleep(2)
 
-        sleep(2)
+            print "Going backwards"
+            GPIO.output(Motor1A, GPIO.LOW)
+            GPIO.output(Motor1B, GPIO.HIGH)
+            GPIO.output(Motor1E, GPIO.HIGH)
 
-        print "Now stop"
-        GPIO.output(Motor1E, GPIO.LOW)
-        GPIO.output(Motor2E, GPIO.LOW)
+            GPIO.output(Motor2A, GPIO.LOW)
+            GPIO.output(Motor2B, GPIO.HIGH)
+            GPIO.output(Motor2E, GPIO.HIGH)
 
-        GPIO.cleanup()
-        return
+            sleep(2)
 
-    return motor_test()
+            print "Now stop"
+            GPIO.output(Motor1E, GPIO.LOW)
+            GPIO.output(Motor2E, GPIO.LOW)
+
+            GPIO.cleanup()
+            return
+
+        return motor_test()
