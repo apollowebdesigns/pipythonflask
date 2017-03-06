cf718391f7206ccea1972a89ce1923a834afbdb3
Andy Evans <andrewevans0@outlook.com>
2017-03-06 21:53:03 +0000

    changes added to controller

--- static/scripts/MapController.js
+++ static/scripts/MapController.js
@@ -1,3 +1,15 @@
 angular
     .module('app')
     .controller('MapController', MapController);
+
+MapController.$inject = ['$scope'];
+
+function MapController ($scope) {
+    angular.extend($scope, {
+                exeter: {
+                    lat: 50.7184,
+                    lng: -3.5339,
+                    zoom: 6
+                }
+            });
+}
