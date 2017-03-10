bc9c2917587c91ac1b32002c49bd20d728e7fab5
Andy Evans <andrewevans0@outlook.com>
2017-03-10 17:15:57 +0000

    more modules made

--- flasktest.py
+++ flasktest.py
@@ -5,48 +5,14 @@ import time
 from time import sleep
 from flask_cors import CORS, cross_origin
 import light
+import motorforwards
 GPIO.setmode(GPIO.BOARD)
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/hits/forwards', methods=['GET'])
 def move_forwards():
-
-    GPIO.setmode(GPIO.BOARD)
-    
-    Motor1A = 16
-    Motor1B = 18
-    Motor1E = 22
-    
-    Motor2A = 23
-    Motor2B = 21
-    Motor2E = 19
-
-    GPIO.setup(Motor1A,GPIO.OUT)
-    GPIO.setup(Motor1B,GPIO.OUT)
-    GPIO.setup(Motor1E,GPIO.OUT)
-    
-    GPIO.setup(Motor2A,GPIO.OUT)
-    GPIO.setup(Motor2B,GPIO.OUT)
-    GPIO.setup(Motor2E,GPIO.OUT)
-    
-    print "Going forwards"
-    GPIO.output(Motor1A,GPIO.HIGH)
-    GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor1E,GPIO.HIGH)
-    
-    GPIO.output(Motor2A,GPIO.HIGH)
-    GPIO.output(Motor2B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.HIGH)
-    
-    sleep(10)
-
-    print "Now stop"
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
-    
-    GPIO.cleanup()
-    return "test"
+    return motorforwards.move()
 
 
 
