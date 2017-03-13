3a8833ae49dc9176575eae82691d8ba7ecc9fab5
Andy Evans <andrewevans0@outlook.com>
2017-03-13 19:28:54 +0000

    adding some standard themes

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,2 +1,12 @@
 angular
     .module('app', ['leaflet-directive']);
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
