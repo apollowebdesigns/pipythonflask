6edfd727b8746300d2fb14989631853b1b48d145
Andy Evans <andrewevans0@outlook.com>
2017-03-13 19:54:59 +0000

    scripts and stuff added

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,12 +1,2 @@
 angular
     .module('app', ['leaflet-directive']);
-
-
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
