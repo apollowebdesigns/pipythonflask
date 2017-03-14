adc608d985d7d94414a7240ae21a9ed83c2e5935
Andy Evans <andrewevans0@outlook.com>
2017-03-14 20:54:13 +0000

    angular leaflet directive added

--- static/scripts/MapController.js
+++ static/scripts/MapController.js
@@ -5,37 +5,20 @@ angular
 MapController.$inject = ['$scope'];
 
 function MapController ($scope) {
-    var mainMarker = {
-        lat: 51,
-        lng: 0,
-        focus: true,
-        message: "Hey, drag me if you want",
-        draggable: true
-    };
-
     angular.extend($scope, {
-        london: {
-            lat: 51.505,
-            lng: -0.09,
+        chicago: {
+            lat: 41.85,
+            lng: -87.65,
             zoom: 8
         },
         markers: {
-            mainMarker: angular.copy(mainMarker)
-        },
-        position: {
-            lat: 51,
-            lng: 0
-        },
-        events: { // or just {} //all events
-            markers:{
-                enable: [ 'dragend' ]
-                //logic: 'emit'
-            }
+            m1: {
+                lat: 41.85,
+                lng: -87.65,
+                message: "I'm a static marker with defaultIcon",
+                focus: false,
+                opacity: 1
+            },
         }
-    });
-
-    $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
-        $scope.position.lat = args.model.lat;
-        $scope.position.lng = args.model.lng;
     });
 }
