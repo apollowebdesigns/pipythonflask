7c0485025cf97bf7fb1e6ffe9799eb22bac1f4a3
Andy Evans <andrewevans0@outlook.com>
2017-03-14 20:40:51 +0000

    changes added to the map controller

--- static/scripts/MapController.js
+++ static/scripts/MapController.js
@@ -5,11 +5,37 @@ angular
 MapController.$inject = ['$scope'];
 
 function MapController ($scope) {
+    var mainMarker = {
+        lat: 51,
+        lng: 0,
+        focus: true,
+        message: "Hey, drag me if you want",
+        draggable: true
+    };
+
     angular.extend($scope, {
-                exeter: {
-                    lat: 50.7184,
-                    lng: -3.5339,
-                    zoom: 6
-                }
-            });
+        london: {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 8
+        },
+        markers: {
+            mainMarker: angular.copy(mainMarker)
+        },
+        position: {
+            lat: 51,
+            lng: 0
+        },
+        events: { // or just {} //all events
+            markers:{
+                enable: [ 'dragend' ]
+                //logic: 'emit'
+            }
+        }
+    });
+
+    $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
+        $scope.position.lat = args.model.lat;
+        $scope.position.lng = args.model.lng;
+    });
 }
