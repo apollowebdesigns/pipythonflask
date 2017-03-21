a1ec8ea65a10377a95081928a73fb4f5fde8309b
Andy Evans <andrewevans0@outlook.com>
2017-03-21 19:13:45 +0000

    changes made to view

--- static/scripts/driveService.js
+++ static/scripts/driveService.js
@@ -7,6 +7,7 @@ driveService.$inject = ['$http', '$log'];
 function driveService ($http, $log) { 
     this.driveData = _driveData;
     this.driveForwards = _driveForwards;
+    this.driveBackwards = _driveBackwards;
     var uniqueIP = "192.168.1.69";
     var uniqueIPparents = "192.168.1.74";
     var redSdCardIp = "192.168.1.73";
@@ -25,6 +26,15 @@ function driveService ($http, $log) {
         $http.get("/hits/forwards")
         .then(function(response) {
             console.log('fowards hit');
+            this.requestedData = response.data;
+        });
+    }
+
+    function _driveBackwards() {
+        $log.info('fowards function entered');
+        $http.get("/hits/backwards")
+        .then(function(response) {
+            console.log('backwards hit');
             this.requestedData = response.data;
         });
     }
