63b138443607271dbcdd78b2eb124d5c8e7d9a03
Andy Evans <andrewevans0@outlook.com>
2017-03-21 19:04:36 +0000

    motor backwards added

---
+++ motorbackwards.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+import RPi.GPIO as GPIO
+import datetime
+import time
+from time import sleep
+
+def move():
+
+    GPIO.setmode(GPIO.BOARD)
+    
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+    
+    Motor2A = 23
+    Motor2B = 21
+    Motor2E = 19
+
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Motor2E,GPIO.OUT)
+
+    print "Going backwards"
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2E,GPIO.HIGH)
+    
+    sleep(10)
+
+    print "Now stop"
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+    
+    GPIO.cleanup()
+    return "test"
