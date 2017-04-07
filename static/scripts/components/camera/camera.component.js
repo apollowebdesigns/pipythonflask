a5656fefd2eebf0db1ed3a48fbd35ed4b19e1651
Andy Evans <andrewevans0@outlook.com>
2017-04-07 19:37:38 +0100

    quick code cleanup

--- static/scripts/components/camera/camera.component.js
+++
@@ -1,6 +0,0 @@
-angular
-    .module('app')
-    .component('cameraComponent', {
-  template: '<div class="parallax-container valign-wrapper"><div class="parallax"><iframe src="http://192.168.1.73:2222/html/min.php" frameborder="0" class="iframe"></iframe></div></div>',
-  controller: function() {}
-});
--- static/scripts/components/main.component.js
+++
@@ -1,8 +0,0 @@
-angular
-    .module('app')
-    .component('mainComponent', {
-  template: '<h1>Home</h1><p>Hello, {{ $ctrl.user.name }} !</p>',
-  controller: function() {
-    this.user = {name: 'world'};
-  }
-});
