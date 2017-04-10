692c97e121ba931a300c99594dbb026cbb9a2f32
Andy Evans <andrewevans0@outlook.com>
2017-04-10 22:05:41 +0100

    motor backwards added

--- motorbackwards.py
+++ motorbackwards.py
@@ -12,30 +12,19 @@ def move():
     Motor1B = 18
     Motor1E = 22
     
-    Motor2A = 23
-    Motor2B = 21
-    Motor2E = 19
-
     GPIO.setup(Motor1A,GPIO.OUT)
     GPIO.setup(Motor1B,GPIO.OUT)
     GPIO.setup(Motor1E,GPIO.OUT)
     
-    GPIO.setup(Motor2A,GPIO.OUT)
-    GPIO.setup(Motor2B,GPIO.OUT)
-    GPIO.setup(Motor2E,GPIO.OUT)
-
-    print "Going backwards"
-    GPIO.output(Motor1A,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.HIGH)
+    print "Turning motor on"
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
     GPIO.output(Motor1E,GPIO.HIGH)
     
     sleep(2)
-
-    print "Now stop"
+    
+    print "Stopping motor"
     GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
     
     GPIO.cleanup()
     return "test"
