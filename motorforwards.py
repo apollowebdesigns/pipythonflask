dab066074515ac6d902b42dcd565452e880a1038
Andy Evans <andrewevans0@outlook.com>
2017-05-07 07:58:00 +0100

    changes made to move forwards and backwards

--- motorforwards.py
+++ motorforwards.py
@@ -16,21 +16,14 @@ def move():
     GPIO.setup(Motor1B,GPIO.OUT)
     GPIO.setup(Motor1E,GPIO.OUT)
     
-    print "Going forwards"
+    print "Turning motor on"
     GPIO.output(Motor1A,GPIO.HIGH)
     GPIO.output(Motor1B,GPIO.LOW)
     GPIO.output(Motor1E,GPIO.HIGH)
     
     sleep(2)
     
-    print "Going backwards"
-    GPIO.output(Motor1A,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.HIGH)
-    GPIO.output(Motor1E,GPIO.HIGH)
-    
-    sleep(2)
-    
-    print "Now stop"
+    print "Stopping motor"
     GPIO.output(Motor1E,GPIO.LOW)
     
     GPIO.cleanup()
