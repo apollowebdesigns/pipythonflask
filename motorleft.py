3936b78f9ef548a088684bcfc90aa30ae6e5c033
Andy Evans <andrewevans0@outlook.com>
2017-05-07 09:04:24 +0100

    motor directions added

---
+++ motorleft.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+import RPi.GPIO as GPIO
+import datetime
+import time
+from time import sleep
+
+def move():
+
+    GPIO.setmode(GPIO.BOARD)
+    
+    Motor2A = 11
+    Motor2B = 13
+    Motor2E = 15
+    
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Motor2E,GPIO.OUT)
+    
+    print "Going backwards"
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2E,GPIO.HIGH)
+    
+    sleep(2)
+    
+    print "Now stop"
+    GPIO.output(Motor2E,GPIO.LOW)
+    
+    GPIO.cleanup()
+    return "test"
