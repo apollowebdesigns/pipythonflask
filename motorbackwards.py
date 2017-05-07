3b6c344cb744f5d6dd93a4abbc550a98f04d42fb
Andy Evans <andrewevans0@outlook.com>
2017-05-07 08:58:59 +0100

    motor changes added

--- motorbackwards.py
+++ motorbackwards.py
@@ -12,6 +12,10 @@ def move():
     Motor1B = 18
     Motor1E = 22
     
+    Motor2A = 16
+    Motor2B = 18
+    Motor2E = 22
+
     GPIO.setup(Motor1A,GPIO.OUT)
     GPIO.setup(Motor1B,GPIO.OUT)
     GPIO.setup(Motor1E,GPIO.OUT)
@@ -20,10 +24,6 @@ def move():
     GPIO.output(Motor1A,GPIO.LOW)
     GPIO.output(Motor1B,GPIO.HIGH)
     GPIO.output(Motor1E,GPIO.HIGH)
-
-    Motor2A = 16
-    Motor2B = 18
-    Motor2E = 22
     
     GPIO.setup(Motor2A,GPIO.OUT)
     GPIO.setup(Motor2B,GPIO.OUT)
@@ -38,6 +38,7 @@ def move():
     
     print "Now stop"
     GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
     
     GPIO.cleanup()
     return "test"
