1e86ab82727fe6ec1577b9218b217bd22ccc5967
Andy Evans <andrewevans0@outlook.com>
2017-05-14 09:01:35 +0100

    changes made to the motor

--- motorleft.py
+++ motorleft.py
@@ -8,9 +8,22 @@ def move():
 
     GPIO.setmode(GPIO.BOARD)
     
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+
     Motor2A = 11
     Motor2B = 13
     Motor2E = 15
+
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    
+    print "Turning motor on"
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
     
     GPIO.setup(Motor2A,GPIO.OUT)
     GPIO.setup(Motor2B,GPIO.OUT)
