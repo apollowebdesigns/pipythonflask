7bdcceeea0f1b3d4ba65b48780f611b447665c5b
Andy Evans <andrewevans0@outlook.com>
2017-05-14 22:19:12 +0100

    sdho;

--- static/scripts/jquery/camera.js
+++ static/scripts/jquery/camera.js
@@ -5,7 +5,7 @@
         var lastPosition = currentUrl.indexOf(":");
         var removeSlash = currentUrl.length - 2;
         var secondUrl = currentUrl.toString().slice(0, removeSlash);
-        var calcUrl = secondUrl.concat(":2222/html/min.php");
+        var calcUrl = secondUrl.concat(String.raw(":2222/html/min.php"));
         $("#camera-screen").attr("src", calcUrl);
     });
 })(jQuery);
