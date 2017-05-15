d0a4fe5f2cb2b98e4442e373e95a53d9ebe0e23a
Andy Evans <andrewevans0@outlook.com>
2017-05-15 19:42:59 +0100

    removing logging

--- static/scripts/jquery/camera.js
+++ static/scripts/jquery/camera.js
@@ -5,14 +5,8 @@
         var lastPosition = currentUrl.indexOf(":");
         var removeSlash = currentUrl.length - 1;
         var secondUrl = currentUrl.toString().slice(0, removeSlash);
-        console.log("the second url is");
-        console.log(secondUrl);
         var rawness = String.raw`:2222/html/min.php`;
-        console.log("rawrrrr");
-        console.log(rawness);
         var calcUrl = secondUrl.concat(rawness);
-        console.log("calcUrl");
-        console.log(calcUrl)
         $("#camera-screen").attr("src", calcUrl);
     });
 })(jQuery);
