caa7b072c9cee98744c9066dfc03d47c2b192823
Andy Evans <andrewevans0@outlook.com>
2017-05-28 08:39:00 +0100

    multiple markers added

--- static/scripts/MapController.js
+++ static/scripts/MapController.js
@@ -6,104 +6,62 @@ MapController.$inject = ['$scope', '$log'];
 
 function MapController ($scope, $log) {
 
-    var currentLat = 51;
-    var currentLng = 51;
-
-    initGeolocation();
-
-    function initGeolocation() {
-        if(navigator.geolocation) {
-           // Call getCurrentPosition with success and failure callbacks
-           navigator.geolocation.getCurrentPosition( success, fail );
-        }
-        else {
-           $log.error("browser not supported");
-        }
-     }
-
-     function success(position) {
-         currentLat = position.coords.longitude;
-         currentLng = position.coords.latitude
-     }
-
-     function fail() {
-        $log.error("mapping current position failed");
-     }
-
-
-    var mainMarker = {
-        lat: 51,
-        lng: 0,
-        focus: true,
-        message: "Hey, drag me if you want",
-        draggable: true
-    };
-
-    var secondMarker = {
-        lat: 51,
-        lng: 2,
-        focus: true,
-        message: "Hey, drag me if you want",
-        draggable: true
-    };
-
     angular.extend($scope, {
-        london: {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 8
-        },
-        markers: {
-            mainMarker: angular.copy(mainMarker)
-        },
-        position: {
-            lat: 51,
-            lng: 0
-        },
-        events: { // or just {} //all events
-            markers:{
-                enable: [ 'dragend' ]
-                //logic: 'emit'
-            }
-        }
-    }, {
-        exeter: {
-            lat: 50.505,
-            lng: -0.09,
-            zoom: 8
-        },
-        markers: {
-            secondMarker: angular.copy(secondMarker)
-        },
-        position: {
-            lat: 51,
-            lng: 3
-        },
-        events: { // or just {} //all events
-            markers:{
-                enable: [ 'dragend' ]
-                //logic: 'emit'
-            }
-        }
-    });
+                london: {
+                    lat: 51.505,
+                    lng: -0.09,
+                    zoom: 8
+                },
 
-    angular.extend($scope, {
-        currentLocation: {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 8
-        },
-        markers: {
-            mainMarker: angular.copy(mainMarker)
-        },
-        position: {
-            lat: 51,
-            lng: 0
-        }
-    });
+                markers: {
 
-    $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
-        $scope.position.lat = args.model.lat;
-        $scope.position.lng = args.model.lng;
-    });
+                    stoke: {
+                        group: 'london',
+                        lat: 51.5615,
+                        lng: -0.0731,
+                        label: {
+                            message: "Stoke",
+                            options: {
+                                noHide: true
+                            }
+                        }
+                    },
+
+                    dalston: {
+                        group: 'london',
+                        lat: 51.545,
+                        lng: -0.070,
+                        label: {
+                            message: "Dalston",
+                            options: {
+                                noHide: true
+                            }
+                        }
+                    },
+
+                    wandsworth: {
+                        group: 'london',
+                        lat: 51.4644,
+                        lng:-0.1924,
+                        label: {
+                            message: "Wandsworth",
+                            options: {
+                                noHide: true
+                            }
+                        }
+                    },
+
+                    battersea: {
+                        group: 'london',
+                        lat: 51.4638,
+                        lng: -0.1677,
+                        label: {
+                            message: "Battersea",
+                            options: {
+                                noHide: true
+                            }
+                        }
+                    }
+                },
+            });
 }
